33dd32413056068645ec7402c9f903c538c4447f
Lucky-kun <yurilucchetti12@gmail.com>
2022-11-29 20:43:14 +0100

    Bat
    Bat per pull e push

---
+++ push.bat
@@ -0,0 +1,18 @@
+@echo off
+
+set name=%~1
+set branch=%~2
+
+if "%name%" == "" (
+    set name=base
+)
+
+if NOT "%branch%" == "" (
+    git checkout "%branch%"
+) else (
+    set branch=master
+)
+
+git add *
+git commit -m "%name%"
+git push origin "%branch%"
